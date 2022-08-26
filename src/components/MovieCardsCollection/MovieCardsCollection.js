ca2d9c39211025554a247592bdc2c00dbf03274d
Jordan Farelli <jordanfarelli@gmail.com>
2022-08-26 15:08:37 -0600

    Tweaded css styling for MovieCardsCollection

--- src/components/MovieCardsCollection/MovieCardsCollection.js
+++ src/components/MovieCardsCollection/MovieCardsCollection.js
@@ -1,10 +1,11 @@
 import './MovieCardsCollection.css'
 import MovieCard from '../MovieCard/MovieCard'
 
-const MovieCardsCollection = ( { individualMovieCard } ) => {
-  const movieCard = individualMovieCard.map( card => {
+const MovieCardsCollection = ( { movieCards } ) => {
+  const movieCard = movieCards.map( card => {
     return (
       <MovieCard 
+      title={ card.title }
       posterPath={ card.poster_path }
       id={ card.id }
       key={ card.id }
@@ -14,6 +15,7 @@ const MovieCardsCollection = ( { individualMovieCard } ) => {
   return (
     <div className="movie-card-collection">
       { movieCard }
+      { !movieCards.length && <h1 className='error-message'>Our Appologies, but our servers are temorarily down. Plesae try again later.</h1> }
     </div>
   )
 
