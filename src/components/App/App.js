ecbc3fc389f1a5e7a4a1e44f84eb013a02d34371
Jordan Farelli <jordanfarelli@gmail.com>
2022-08-27 11:22:38 -0600

    Added functionality to select singel movie

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,8 +5,7 @@ import fetchData from '../../apiCalls.js'
 import Nav from  '../Nav/Nav'
 import Footer from '../Footer/Footer'
 import MovieCardsCollection from '../MovieCardsCollection/MovieCardsCollection'
-
-// import data from "../../fakedata"
+import MovieDetails from '../MovieDetails/MovieDetails'
 
 
 class App extends React.Component {
@@ -15,23 +14,38 @@ class App extends React.Component {
     super( )
     this.state = 
       {
-        movieCards: [ ]
+        movieCards: [ ],
+        movieSelected: ''
       }
+    }
+    
+    componentDidMount = ( ) => {
+      fetchData( '/movies' )
+      .then( data => this.setState( { movieCards: data.movies } ) )
   }
 
-  componentDidMount( ) {
-    fetchData( 'movies' )
-    .then( data => this.setState( { movieCards: data.movies } ) )
-  }
+    displaySingleMovie = ( id ) => {
+        const singleMovie = this.state.movieCards.find( movie => movie.id === id  )
+        this.setState( { movieSelected: singleMovie } )
+    }
 
   render( ) {
+    let display;
+    if ( this.state.movieSelected ) {
+      display = <MovieDetails selectedMovie={ this.state.movieSelected }/> 
+    } else {
+      display = <MovieCardsCollection 
+      movieCards={ this.state.movieCards }
+      displaySingleMovie={ this.displaySingleMovie }/>
+    }
+
+
     return (
       <div className='app-container'>
 
         <Nav />
 
-        <MovieCardsCollection 
-          movieCards={ this.state.movieCards }/>
+        { display }
 
         <Footer />
 
