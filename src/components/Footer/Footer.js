422808690a52103c7e39c17b95a4ea6afa9d176d
Anthony Shellman <atshellman@gmail.com>
2022-08-28 21:11:03 -0600

    Update alt property values

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,13 +7,13 @@ const Footer = ( ) => {
     <div className="footer-container">
       <div className='jordan-container'>
         <h4>Jordan Farelli</h4>
-        <a 
+        <a
           href='https://www.linkedin.com/in/jordan-farelli/'
           target="_blank"
           rel="noreferrer">
           <img 
             src={ linkedInIcon }
-            alt='lined-in-icon'
+            alt='linkedin-icon'
             className='linkedin-icon'/>
         </a>
         <a 
@@ -29,16 +29,16 @@ const Footer = ( ) => {
 
       <div className='anthony-container'>
         <h4>Anthony Shellman</h4>
-        <a 
+        <a
           href='https://www.linkedin.com/in/anthonyshellman/'
           target="_blank"
           rel="noreferrer">
           <img 
             src={ linkedInIcon }
-            alt='lined-in-icon'
+            alt='linkedin-icon'
             className='linkedin-icon'/>
         </a>
-        <a 
+        <a
           href='https://github.com/Ant-Shell'
           target="_blank"
           rel="noreferrer">
