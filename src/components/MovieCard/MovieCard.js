a2aa0c65935aaa694cbfa545e579de10f1a1e80d
Anthony Shellman <atshellman@gmail.com>
2022-08-29 17:59:19 -0600

    Import link component, add to return

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,16 +1,19 @@
 import './MovieCard.css'
+import { Link } from 'react-router-dom';
 
 const MovieCard = ( { title, posterPath, id, displaySingleMovie, toggleHomeButton } ) => {
   
   
   return (
     <div className='individual-movie-card' key={ id }>
+      <Link to={`/${id}`}>
       <img 
         className='movie-card-image' 
         src={ posterPath } 
         alt={ `Movie poster for ${ title }` }
         onClick={ (  ) => displaySingleMovie( id ) }
         />
+        </Link>
     </div>
   )
 
