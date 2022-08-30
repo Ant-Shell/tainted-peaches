60d894c1bbb23317f7ca0982b7ad9a5638ec8acc
Anthony Shellman <atshellman@gmail.com>
2022-08-29 18:01:02 -0600

    Import link and add to return

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,6 @@
 import './Nav.css'
 import tainted from '../../assets/tainted.jpeg'
+import { Link } from "react-router-dom"
 
 const Nav = ( { homeButton, returnHome } ) => {
   return (
@@ -9,7 +10,8 @@ const Nav = ( { homeButton, returnHome } ) => {
         alt='tainted-peach-logo' 
         className="tainted-peach-image"/>
       <h1 className="nav-title">Tainted Peaches</h1>
-      { homeButton && <button className='nav-home-button' onClick={() => returnHome()}>Take Me Home</button>}
+      {/* { homeButton && <button className='nav-home-button' onClick={() => returnHome()}>Take Me Home</button>} */}
+      <Link to={'/'}onClick={() => returnHome()}>{ homeButton && <button className='nav-home-button'>Take Me Home</button>}</Link>
     </div>
   )
 
